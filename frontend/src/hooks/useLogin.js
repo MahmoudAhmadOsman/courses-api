9415f1c4352edabbc5c2de37254109600d12efb9
Mahmoud Osman <34916796+MahmoudAhmadOsman@users.noreply.github.com>
2024-06-29 17:31:27 -0500

    Update useLogin.js

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -13,7 +13,7 @@ export const useLogin = () => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch(BASE_URL + "/api/user/login", {
+    const response = await fetch(BASE_URL + "/api/users/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })
